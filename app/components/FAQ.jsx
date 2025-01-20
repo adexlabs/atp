4360924a8943ff65037385b389b2fd6635575710
adexlabs <contact@adexlabs.com>
2025-01-20 12:35:01 +0530

    .

--- app/components/FAQ.jsx
+++ app/components/FAQ.jsx
@@ -1,10 +1,24 @@
 // Import React and CSS
 import React, { useState } from "react";
 import Section_Banner from "./Section_Banner";
-import bnnrImg from '~/assets/d-hero-seagate (1).jpg';
+import bnnrImg from '~/assets/faq-2655310_640.jpg';
+import {useShopQuery, gql} from '@shopify/hydrogen';
 export default function FAQ () {
   const [activeIndex, setActiveIndex] = useState(null);
+  const {data} = useShopQuery({
+    query: gql`
+      query FAQMenu {
+        menu(handle: "faq") {
+          items {
+            title
+            url
+          }
+        }
+      }
+    `,
+  });
 
+  const menuItems = data?.menu?.items || [];
   const faqData = [
     {
       question: "What is the end-to-end data recovery services process?",
