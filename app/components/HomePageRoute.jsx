a171d22b01696706e4a37eb2a0622a2f59378b91
adexlabs <contact@adexlabs.com>
2025-01-20 12:05:20 +0530

    .

--- app/components/HomePageRoute.jsx
+++ app/components/HomePageRoute.jsx
@@ -41,10 +41,10 @@ export default function HomePageRoute() {
   return (
     <>
     <Routes>
-<Route><Link to="/imgtextsection" element={<ImageTextSection/>}/></Route>
-<Route><Link to="/slider" element={<LogoSlider/>}/></Route>
+<Route path="/imgtextsection" element={<ImageTextSection/>}/>
+<Route path="/slider" element={<LogoSlider/>}/>
 <div className="text-banner">
-<Route><Link to="/banner" element={ <NewBanner
+<Route path="/banner" element={ <NewBanner
        title="Lost your data? Don't fret."
        caption=""
        buttonText="Have a Query?"
@@ -53,11 +53,11 @@ export default function HomePageRoute() {
        backgroundImage="" // Background image URL
        highlightWordCount={2} 
        paragraph="ATP Data Services to the rescue to get your indispensable data back! Our team of data recovery experts has many years of experience working in the storage and recovery industry.  We are dedicated to providing the best possible service to our clients. We pride ourselves on our professionalism, confidentiality, and commitment to excellence.  You can count on us!"
-      />}/></Route>
+      />}/>
     </div>
 
     <div className="digital-assets">
-    <Route><Link to="/banner" element={<NewBanner
+    <Route path="/banner" element={<NewBanner
   title="Recover your most important digital assets today."
   caption=""
   buttonText="Get Started"
@@ -65,9 +65,9 @@ export default function HomePageRoute() {
   bannerStyle={{ height: "520px", backgroundColor: "#f6f6f8" }} // adjustable banner style
   backgroundImage={recover} 
   highlightWordCount={0} 
-/>}/></Route>
+/>}/>
   </div>
-  <Route><Link to="/trustbadges" element={<TrustBadges badges={customBadges} count={4} />}/></Route>
+  <Route path="/trustbadges" element={<TrustBadges badges={customBadges} count={4} />}/>
   </Routes>
     </>
   )
