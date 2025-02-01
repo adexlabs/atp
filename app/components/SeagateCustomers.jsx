c2e36a50bfcf08ec95070b774bb2866634404215
adexlabs <contact@adexlabs.com>
2025-02-01 15:35:21 +0530

    Updated

--- app/components/SeagateCustomers.jsx
+++ app/components/SeagateCustomers.jsx
@@ -1,21 +1,20 @@
 import React from 'react'
 import SegateBanner from './SegateComponents/SegateBanner'
 import logoImg from '~/assets/logo-seagate.png';
+
+import bnnrImg2 from '~/assets/d-hero-seagate.jpg';
 export default function SeagateCustomers  () {
   return (
     <>
+    <div className='segate-banner'>
    <SegateBanner
  title="Welcome to Our Service"
  buttonText="Get Started"
  onButtonClick={() => alert("Button clicked")}
- backgroundImage="https://example.com/your-background-image.jpg"
+ backgroundImage={bnnrImg2}
  image={logoImg}  // Image shown above the caption
  highlightWordCount={2}
-
-/>
-
-
-
+/></div>
     </>
   )
 }
