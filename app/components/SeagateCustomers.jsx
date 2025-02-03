7601137782274fde683e8092f3c75fdf872eedcf
adexlabs <contact@adexlabs.com>
2025-02-03 10:37:30 +0530

    UPDATED

--- app/components/SeagateCustomers.jsx
+++ app/components/SeagateCustomers.jsx
@@ -25,6 +25,31 @@ export default function SeagateCustomers  () {
 <ProductTrustBadges/>
 
 <LogoSlider/>
+
+<div className="text-banner">
+    <NewBanner
+       title="Lost your data? Don't fret."
+       caption=""
+       buttonText="Have a Query?"
+       onButtonClick={handleButtonClick}
+       bannerStyle={{ height: "390px", backgroundColor: "" }} // adjustable banner style
+       backgroundImage="" // Background image URL
+       highlightWordCount={2} 
+       paragraph="ATP Data Services to the rescue to get your indispensable data back! Our team of data recovery experts has many years of experience working in the storage and recovery industry.  We are dedicated to providing the best possible service to our clients. We pride ourselves on our professionalism, confidentiality, and commitment to excellence.  You can count on us!"
+      />
+    </div>
+
+    <div className="digital-assets">
+<NewBanner
+  title="Recover your most important digital assets today."
+  caption=""
+  buttonText="Get Started"
+  onButtonClick={() => alert("Button clicked")}
+  bannerStyle={{ height: "520px", backgroundColor: "#f6f6f8" }} // adjustable banner style
+  backgroundImage={recover} 
+  highlightWordCount={0} 
+/>
+  </div>
     </>
   )
 }
