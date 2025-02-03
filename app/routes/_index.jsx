1fafa85125b4bae783e6a7911faf4001535738b6
adexlabs <contact@adexlabs.com>
2025-02-03 11:59:17 +0530

    Updated

--- app/routes/_index.jsx
+++ app/routes/_index.jsx
@@ -2,6 +2,7 @@ import {defer} from '@shopify/remix-oxygen';
 import {Await, useLoaderData, Link} from '@remix-run/react';
 import {Suspense} from 'react';
 import {Image, Money} from '@shopify/hydrogen';
+import Collection from './collections.$handle';
 
 /**
  * @type {MetaFunction}
@@ -66,6 +67,7 @@ export default function Homepage() {
 
   return (
     <div className="home page-width">
+      <Collection/>
       <FeaturedCollection collection={data.featuredCollection} />
       <RecommendedProducts products={data.recommendedProducts} />
     </div>
