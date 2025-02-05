daa019c35a83aa9d3d74a333392edb4063c59a42
adexlabs <contact@adexlabs.com>
2025-02-05 11:11:36 +0530

    Updated

--- app/routes/segatecustomers.jsx
+++ app/routes/segatecustomers.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import SeagateCustomers from '~/components/SeagateCustomers'
 
-export default function segate_customers () {
+export default function segatecustomers () {
   return <SeagateCustomers/>
 }
 
