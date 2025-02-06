23d60abcbf0ffbb0ef2a2a0046f9686e4e6ec7f8
adexlabs <contact@adexlabs.com>
2025-02-06 15:32:01 +0530

    Updated

--- app/components/FAQ.jsx
+++ app/components/FAQ.jsx
@@ -129,7 +129,7 @@ export default function FAQ () {
           
      <Section_Banner
   backgroundImage={bnnrImg} 
-  height="450px"
+  height="400px"
   text=""
   textColor="#fff"
   textAlign="center"
