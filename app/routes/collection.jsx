a7ccffca87c14075fd339b36268bde2a57d095bd
adexlabs <contact@adexlabs.com>
2025-02-11 10:22:39 +0530

    Collection Route set

---
+++ app/routes/collection.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Collection from './collections.$handle'
+
+export default function collection () {
+  return (
+    <div>
+        <Collection/>
+    </div>
+  )
+}
+
+ 
