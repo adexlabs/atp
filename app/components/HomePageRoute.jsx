15d5b65435d7e65cdb9c6bb5dc68323ffc2dc2b4
adexlabs <contact@adexlabs.com>
2025-02-12 12:31:37 +0530

    Updated

--- app/components/HomePageRoute.jsx
+++ app/components/HomePageRoute.jsx
@@ -42,8 +42,8 @@ export default function HomePageRoute() {
   return (
     <>
 <ImageTextSection/>
-{/* <LogoSlider/> */}
-{/* <div className="text-banner">
+<LogoSlider/>
+<div className="text-banner">
     <NewBanner
        title="Lost your data? Don't fret."
        caption=""
@@ -54,7 +54,7 @@ export default function HomePageRoute() {
        highlightWordCount={2} 
        paragraph="ATP Data Services to the rescue to get your indispensable data back! Our team of data recovery experts has many years of experience working in the storage and recovery industry.  We are dedicated to providing the best possible service to our clients. We pride ourselves on our professionalism, confidentiality, and commitment to excellence.  You can count on us!"
       />
-    </div> */}
+    </div>
 
     <div className="digital-assets">
 <NewBanner
--- app/components/ProductPageRoute.jsx
+++ app/components/ProductPageRoute.jsx
@@ -19,7 +19,7 @@ export default function ProductPageRoute (){
        paragraph="ATP Data Services to the rescue to get your indispensable data back! Our team of data recovery experts has many years of experience working in the storage and recovery industry.  We are dedicated to providing the best possible service to our clients. We pride ourselves on our professionalism, confidentiality, and commitment to excellence.  You can count on us!"
       />
     </div>
-    {/* <LogoSlider/> */}
+    <LogoSlider/>
     </>
   )
 }
