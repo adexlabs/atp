c65e7f30bad7e015026147fa1db96f4b85c02afb
adexlabs <contact@adexlabs.com>
2025-02-13 15:03:28 +0530

    Updated Text

--- app/components/VideoTextSection.jsx
+++ app/components/VideoTextSection.jsx
@@ -8,24 +8,31 @@ export default function VideoTextSection () {
     <div className='video-text'>
         <div className='video-content'>
         <h2 className='v-content-title1'>What Customers are Saying</h2>
-        <p className='v-content'>They really understood my work was super, duper important. 
-            That makes you feel like someone is really taking care of you, that someone has your back.</p>
-        <p className='v-content address'>- Theresa Debono, Photographer, Malta</p>
-        <p className='v-content'>Consumers and even professionals who buy something like a hard drive 
-            probably expect it will work forever.  There are occasions when things fail.  
-            I was happy to call the client and say it's handled, we have your files and are ready to go.</p>
-        <p className='v-content address'>- Dave Prunty, Videographer, Montana, United States</p>
+        
+      
         <h2 className='v-content-title2'>Watch Customer Videos</h2>
         </div>
         <div className='video-wrapper'>
+            <div className='video-text-wrap'>
         <Video
         videoUrl="https://cdn.shopify.com/videos/c/o/v/ce8714bb41f042428c3dfa9e499eaaa2.mp4"
         coverImage={videocoverimg1}
       />
+     <p className='v-content'>They really understood my work was super, duper important. 
+            That makes you feel like someone is really taking care of you, that someone has your back.</p>
+        <h4 className='v-content address'>- Theresa Debono, Photographer, Malta</h4>
+      </div>
+      <div className='video-text-wrap'>
         <Video
         videoUrl="https://cdn.shopify.com/videos/c/o/v/ce8714bb41f042428c3dfa9e499eaaa2.mp4"
         coverImage={videocoverimg2}
       />
+
+<p className='v-content'>Consumers and even professionals who buy something like a hard drive 
+            probably expect it will work forever.  There are occasions when things fail.  
+            I was happy to call the client and say it's handled, we have your files and are ready to go.</p>
+        <h4 className='v-content address'>- Dave Prunty, Videographer, Montana, United States</h4>
+      </div>
         </div>
     </div>
     </>
