f5a55036b1835b0d781a00db5c61cc4fea504c36
adexlabs <contact@adexlabs.com>
2025-02-13 12:28:58 +0530

    Updated

--- app/components/ProductPageRoute.jsx
+++ app/components/ProductPageRoute.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import LogoSlider from './LogoSlider'
 import ProductTrustBadges from './ProductTrustBadges'
 import NewBanner from './NewBanner'
+import VideoTextSection from './VideoTextSection'
 
 export default function ProductPageRoute (){
   return (
     <>
+    <VideoTextSection/>
     <ProductTrustBadges/>
     <LogoSlider/>
     </>
