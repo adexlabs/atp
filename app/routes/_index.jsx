89f65bfbdf6a37482f3e4cde6773150ef7a7b2c0
adexlabs <contact@adexlabs.com>
2025-02-13 11:09:38 +0530

    Video Section

--- app/routes/_index.jsx
+++ app/routes/_index.jsx
@@ -13,6 +13,7 @@ import HomePageRoute from '~/components/HomePageRoute';
 import Banner from '~/components/Banner';
 import ImageSlider from '~/components/ImageSlider';
 import ImageTextSection from '~/components/ImageTextSection';
+import VideoTextSection from '~/components/VideoTextSection';
 /**
  * @type {MetaFunction}
  */
@@ -106,7 +107,7 @@ export default function Homepage() {
     <div className="home">
          <Banner/>
          <ImageSlider images={images} />
-         <ImageTextSection/>
+         <VideoTextSection/>
       {/* <FeaturedProducts products={data.featuredProducts} /> */}
       {/* <FeaturedCollection collection={data.featuredCollection} /> */}
       {/* <RecommendedProducts products={data.recommendedProducts} /> */}
