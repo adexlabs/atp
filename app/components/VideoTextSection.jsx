5844ec9df8fe542e71d59412ed466f254b02a53c
adexlabs <contact@adexlabs.com>
2025-02-13 15:51:55 +0530

    V Updated

--- app/components/VideoTextSection.jsx
+++ app/components/VideoTextSection.jsx
@@ -16,7 +16,7 @@ export default function VideoTextSection () {
         videoUrl="https://cdn.shopify.com/videos/c/o/v/ce8714bb41f042428c3dfa9e499eaaa2.mp4"
         coverImage={videocoverimg1}
       />
-   <div className='video-content'> <p className='v-content'>They really understood my work was super, duper important. 
+   <div className='content'> <p className='v-content'>They really understood my work was super, duper important. 
             That makes you feel like someone is really taking care of you, that someone has your back.</p>
         <h4 className='v-address'>- Theresa Debono, Photographer, Malta</h4></div>
       </div>
@@ -25,7 +25,7 @@ export default function VideoTextSection () {
         videoUrl="https://cdn.shopify.com/videos/c/o/v/ce8714bb41f042428c3dfa9e499eaaa2.mp4"
         coverImage={videocoverimg2}
       />
-      <div className='video-content'>
+      <div className='content'>
         <p className='v-content'>Consumers and even professionals who buy something like a hard drive 
             probably expect it will work forever. There are occasions when things fail.  
             I was happy to call the client and say it's handled, we have your files and are ready to go.</p>
