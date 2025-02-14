fffee9eeaa5db4d1a5bae9a4ddd85cb04c2bb9bf
adexlabs <contact@adexlabs.com>
2025-02-14 16:56:49 +0530

    Updated

--- app/components/LanguageSelector.jsx
+++ app/components/LanguageSelector.jsx
@@ -1,21 +1,19 @@
-import { useEffect, useState } from 'react';
+import { useLoaderData } from '@remix-run/react';
+
+export function loader() {
+  return fetch('/api/languages').then((res) => res.json());
+}
 
 export default function LanguageSelector() {
-  const [languages, setLanguages] = useState([]);
-
-  useEffect(() => {
-    fetch('/api/languages')
-      .then((res) => res.json())
-      .then((data) => setLanguages(data));
-  }, []);
+  const languages = useLoaderData();
 
   return (
     <div>
-      <h3>Choose Language</h3>
+      <h3>🌍 Select Language</h3>
       <select>
         {languages.map((lang) => (
           <option key={lang.isoCode} value={lang.isoCode}>
-            {lang.name} ({lang.isoCode})
+            {lang.name}
           </option>
         ))}
       </select>
