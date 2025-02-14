e6d1077997b4fed02feda2920ee52eab3b21b28c
adexlabs <contact@adexlabs.com>
2025-02-14 17:27:05 +0530

    Updated

--- env.d.ts
+++ env.d.ts
@@ -16,10 +16,14 @@ declare global {
   /**
    * A global `process` object is only available during build to access NODE_ENV.
    */
-  const process: {env: {NODE_ENV: 'production' | 'development'}};
-  PUBLIC_CHECKOUT_DOMAIN=atp-data-services.myshopify.com
+  const process: {env: {
+    NODE_ENV: 'production' | 'development'
+    PUBLIC_CHECKOUT_DOMAIN: string;
+  }};
+  
   interface Env extends HydrogenEnv {
     // declare additional Env parameter use in the fetch handler and Remix loader context here
+    PUBLIC_CHECKOUT_DOMAIN: string;
   }
 }
 
