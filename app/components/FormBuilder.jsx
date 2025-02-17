86bb3307c065dd0ca4ed317d9ea56e66a5f998a2
adexlabs <contact@adexlabs.com>
2025-02-17 15:33:07 +0530

    Updated

--- app/components/FormBuilder.jsx
+++ app/components/FormBuilder.jsx
@@ -1,22 +1,17 @@
-import React from 'react'
-
-export default function FormBuilder  () {
-  return (
-    <div>
-
-<iframe
-      title="Preview Bar"
-      id="preview-bar-iframe"
-      src="https://atp-data-services.myshopify.com/preview_bar"
-      sandbox="allow-same-origin allow-scripts"
-      style={{
-        display: "block", 
-        width: "100%",
-        height: "60px",
-        border: "none",
-      }}
-    />
-    </div>
-  )
-}
-
+export default function FormBuilder() {
+    return (
+      <iframe
+        title="Form Builder"
+        id="form-builder-iframe"
+        src="https://atp-data-services.myshopify.com/preview_bar"
+        sandbox="allow-same-origin allow-scripts"
+        style={{
+          display: "block",
+          width: "100%",
+          height: "600px", // Adjust height as needed
+          border: "none",
+        }}
+      />
+    );
+  }
+  
