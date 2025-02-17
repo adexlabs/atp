8d29241597e6e6384914151a96e9d4d5c2fcf3e1
adexlabs <contact@adexlabs.com>
2025-02-17 15:26:43 +0530

    Form Updated

--- app/components/FormBuilder.jsx
+++ app/components/FormBuilder.jsx
@@ -1,26 +1,22 @@
-// app/components/FormBuilder.jsx
-import { useEffect } from "react";
+import React from 'react'
 
-export default function FormBuilder() {
-  useEffect(() => {
-    // Dynamically load the script needed to initialize the form
-    const script = document.createElement("script");
-    script.src = "https://app.powerfulform.com/api/front/form/76655/send"; // Replace with the correct script URL if needed
-    script.async = true;
-
-    // Append the script to the body of the document
-    document.body.appendChild(script);
-
-    // Cleanup when the component is unmounted
-    return () => {
-      document.body.removeChild(script);
-    };
-  }, []);
-
+export default function FormBuilder  () {
   return (
-    <div className="form-builder-container">
-      <h2>Contact Us</h2>
-      <div className="globo-formbuilder" data-id="NzY2NTU="></div> {/* Embed the form using the provided div */}
+    <div>
+
+<iframe
+      title="Preview Bar"
+      id="preview-bar-iframe"
+      src="https://atp-data-services.myshopify.com/preview_bar"
+      sandbox="allow-same-origin allow-scripts"
+      style={{
+        display: "block", 
+        width: "100%",
+        height: "60px",
+        border: "none",
+      }}
+    />
     </div>
-  );
+  )
 }
+
