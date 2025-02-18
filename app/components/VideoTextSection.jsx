51d63fd14b6c21b97bab584c86933fbb5e6319f6
adexlabs <contact@adexlabs.com>
2025-02-18 13:24:08 +0530

    Updated

--- app/components/VideoTextSection.jsx
+++ app/components/VideoTextSection.jsx
@@ -11,7 +11,7 @@ export default function VideoTextSection () {
         <h2 className='v-content-title2'>Watch Customer Videos</h2>
         </div>
         <div className='video-wrapper'>
-            {/* <div className='video-text-wrap'>
+            <div className='video-text-wrap'>
         <VideoGallery
         videoUrl="https://cdn.shopify.com/videos/c/o/v/dd1de37d3a9e45c095ab5d4ad37d7e34.mp4"
         coverImage={videocoverimg1}
@@ -30,9 +30,9 @@ export default function VideoTextSection () {
             probably expect it will work forever. There are occasions when things fail.  
             I was happy to call the client and say it's handled, we have your files and are ready to go.</p>
         <h4 className='v-address'>- Dave Prunty, Videographer, Montana, United States</h4></div>
-      </div> */}
+      </div>
 
-<VideoSection
+{/* <VideoSection
   videos={[
     { 
       coverImage: "videocoverimg1", 
@@ -47,7 +47,7 @@ export default function VideoTextSection () {
       secondTitle: "- Dave Prunty, Videographer, Montana, United States"
     }
   ]}
-/>
+/> */}
 
         </div>
     </div>
