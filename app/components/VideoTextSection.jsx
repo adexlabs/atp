46c1033979027d31f4260d457ac40310f4b22fd6
adexlabs <contact@adexlabs.com>
2025-02-18 13:18:54 +0530

    Updated

--- app/components/VideoTextSection.jsx
+++ app/components/VideoTextSection.jsx
@@ -35,13 +35,13 @@ export default function VideoTextSection () {
 <VideoSection
   videos={[
     { 
-      coverImage=videocoverimg1, 
+      coverImage="videocoverimg1", 
       videoUrl="https://cdn.shopify.com/videos/c/o/v/dd1de37d3a9e45c095ab5d4ad37d7e34.mp4",
       firstParagraph: "They really understood my work was super, duper important. That makes you feel like someone is really taking care of you, that someone has your back.",
       secondTitle: "- Theresa Debono, Photographer, Malta"
     },
     { 
-      coverImage=videocoverimg2, 
+      coverImage="videocoverimg2", 
        videoUrl="https://cdn.shopify.com/videos/c/o/v/da21d90dfe014d4cb7db92ea09cd15d4.mp4",
       firstParagraph: "Consumers and even professionals who buy something like a hard drive probably expect it will work forever. There are occasions when things fail.  I was happy to call the client and say it's handled, we have your files and are ready to go.",
       secondTitle: "- Dave Prunty, Videographer, Montana, United States"
